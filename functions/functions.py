4655742e5c94d727fa4ea0d338f8cf004b299e76
Denis Kravchenko <pochtastudenta@gmail.com>
2023-08-08 17:30:03 +0200

    All ready

--- functions/functions.py
+++ functions/functions.py
@@ -1,3 +1,4 @@
+import os
 def create_dict(file_):
     cook_book = {}
     with open(file_, encoding='utf-8') as work_file:
@@ -35,6 +36,29 @@ def get_shop_list_by_dishes(dishes, person_count, cook_book):
                     ingredient_for_person[ingredient['ingredient_name']]['quantity'] += int(
                         ingredient['quantity']) * person_count
         else:
-            print(f'Блюда {dish.lower()} нет в кулинарной книге')
+            print(f'Блюда "{dish.capitalize()}" нет в кулинарной книге')
 
     return ingredient_for_person
+
+
+def sort_files(path, file_):
+    result = []
+    for file in file_:
+        with open(f'{path}{file}', encoding='utf-8') as f:
+            read_all_file = f.readlines()
+            strcount = len(read_all_file)
+            file_name = f.name.split('/')[1]
+            item = strcount, file_name, "".join(read_all_file)
+            result.append(item)
+    result.sort()
+    out_file = 'result/result.txt'
+    if os.path.isfile(out_file):
+        os.remove(out_file)
+
+    with open(out_file, 'a', encoding='utf-8') as f:
+        for data in result:
+            count, name, text = data
+            f.write(f'{name}\n')
+            f.write(f'{count}\n')
+            f.write(f'{text}\n')
+
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from functions.functions import create_dict, get_shop_list_by_dishes
+from functions.functions import create_dict, get_shop_list_by_dishes, sort_files
 
 file = 'source/recipes.txt'
 # Задача 1: делаем словарь из файла
 cook_book = create_dict(file)
 
 # Задача 2:
-dish_list = ['Омлет', 'млет']
+dish_list = ['Омлет', 'Фахитос', 'Запеченный картофель']
 persons = 2
 # для наглядности(удобства) напечатаем так
 for keys, values in get_shop_list_by_dishes(dish_list, persons, cook_book).items():
     print(keys, values)
 
 
+#Задача 3
+path = 'source/'
+name_file_list = ['1.txt', '2.txt', '3.txt']
+sort_files(path, name_file_list)
